1247715166e0630b925f4e549f7072f52ef9fd07
rnmalbdrani <raneemalbdr@gmail.com>
2020-11-27 19:33:47 +0300

    first

---
+++ p3.php
@@ -0,0 +1,107 @@
+
+    <?php include('server.php'); ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Like and Dislike system</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <link rel="stylesheet" href="main.css">
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
+      <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/droidarabicnaskh.css">
+       <link rel="stylesheet" href="stylee.css">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/droidarabicnaskh.css">
+    </head>
+    <body>
+      <div class="w3-bar w3-black">
+          <span class=" branding w3-bar-item w3-mobile"> healthy life </span>
+          <span class="w3-right w3-mobile " >
+      <a href="project.html"class="w3-bar-item w3-button w3-mobile w3-hover-red">الرئـيسـية</a>
+      <a href="p2.html"class="w3-bar-item w3-button w3-mobile w3-hover-red">الريـاضة</a>
+      <a href="Food.php"class="w3-bar-item w3-button w3-mobile w3-hover-red">الأنـظمة الغذائـية</a>
+      <a href="mailto:web.contact122@gmail.com"class="w3-bar-item w3-button w3-mobile w3-hover-red">تواصـل معنا</a>
+      </span>
+      </div>
+
+        <div class="w3-container w3-center">
+      <h4 class="w3-animate-opacity"> إذا كنت مقبلاً على ممارسة الرياضة وإدخالها كنشاط أساسي الى يومك، وان كنت تريد تكوين صالة الجيم الخاصة بك داخل منزلك، هناك عدد من الأدوات والمعدات الرياضية الأساسية التي لا يمكن اتمام أي تمرين بدونهم، وسنعرّفك  على أهم المعدات الرياضية اللازمة لإتمام أي تمرين رياضي ناجح</h4>
+
+      <section class="section  w3-black">
+           <div class="w3-container w3-center">
+
+    </div>
+    </section>
+    <section class="section w3-light-grey">
+      <div class="w3-container w3-center">
+
+          <table class="center" >
+
+            <tr>
+              <td class="cc">الأوزان</td>
+              <td >تسمى "​الدمبلز"، وهي على الأغلب الأداة الأكثر أهمية من بين المعدّات التي عليك استخدامها عند التمرين. فعدد التمارين متنوّع جدا، وأنصحك بشراء الأوزان القابلة للتغيير، لتتمكن من زيادة أو إنقاص أقراص الوزن الموجودة عليها، وهي مناسبة أكثر للمساحات الصغيرة في المنزل.</td>
+              <td> <img src="https://striveme.com/img/gallery/lifestylejune1/%D8%A3%D8%AF%D9%88%D8%A7%D8%AA%20%D8%B1%D9%8A%D8%A7%D8%B6%D9%8A%D8%A9%204.jpg" alt="dam"width="300" height="400"></td>
+            </tr>
+            <tr>
+              <td class="cc">حبل القفز</td>
+
+              <td>يُعد حبل القفز من أبسط الأدوات التي يمكنك اقتنائها واستخدامها لأداء تدريب الكارديو الخاص بك، كما يمكنك مزج تدريب قفز الحبل مع تمارين اُخرى لتحفيز ورفع معدّل الأيض لديك.  </td>
+              <td><img src="https://static.toiimg.com/photo/71956822.cms"alt="dam"width="300" height="400"></td>
+            </tr>
+            <tr>
+         <td class="cc">حامل أثقال "البار"</td>
+         <td>اساسي للحصول على جدول تمارين متكامل ويشبه نوعا ما بالدامبلز ولكن الذي يميز البار عن الدامبلز هو انه يمكنك من اداء التمارين وحمل أوزان أكبر لايمكنك السيطرة عليها بإستخدام الدامبلز وحدها ويمكنك استخدامه من دون أوزان لأداء بعض التمارين أيضاً
+      </td>
+         <th><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvcLmfEw9Z1HRKPu7bUZPyKDBlA2AmwT67lg&usqp=CAU" alt="bar"width="300" height="400"></th>
+      </tr>
+
+      <tr>
+         <td class="cc">كرة الحديد Kettlebells</td>
+         <td> تتوفر كرة الحديد بأوزان مختلفة تناسب مستوى لياقتك، وتساعدك على حرق ما لا يقل عن 20 سعر حراري في الدقيقة، وتعد من الطرق البسيطة لتقوية جسمك.
+      </td>
+         <td><img src="https://striveme.com/img/gallery/lifestylejune1/%D8%A3%D8%AF%D9%88%D8%A7%D8%AA%20%D8%B1%D9%8A%D8%A7%D8%B6%D9%8A%D8%A9.jpg" alt="pp"width="300" height="400"></td>
+      </tr>
+      <tr>
+         <td class="cc">بساط التمرين</td>
+         <td> هو بساط تمارين اليوجا، وهو مهم لاعطاءك الراحة والتثبيت والحماية الكافية لظهرك عند أدائك للتمارين الأرضية، عليك أن تختار بساط سميك وغير قابل للإنزلاق على الأرض كرة الحديد بأوزان مختلفة تناسب مستوى لياقتك، وتساعدك على حرق ما لا يقل عن 20 سعر حراري في الدقيقة، وتعد من الطرق البسيطة لتقوية جسمك.
+      </td>
+         <td><img src="https://striveme.com/img/gallery/lifestylejune1/%D8%A3%D8%AF%D9%88%D8%A7%D8%AA%20%D8%B1%D9%8A%D8%A7%D8%B6%D9%8A%D8%A9%205.jpg" alt="pp"width="300" height="400"></td>
+      </tr>
+          </table>
+
+    </body>
+    <body>
+      <div class="posts-wrapper">
+       <?php foreach ($posts as $post): ?>
+       	<div class="post">
+          <?php echo $post['text']; ?>
+          <div class="post-info">
+    	    <!-- if user likes post, style button differently -->
+          	<i <?php if (userLiked($post['id'])): ?>
+          		  class="fa fa-thumbs-up like-btn"
+          	  <?php else: ?>
+          		  class="fa fa-thumbs-o-up like-btn"
+          	  <?php endif ?>
+          	  data-id="<?php echo $post['id'] ?>"></i>
+          	<span class="likes"><?php echo getLikes($post['id']); ?></span>
+
+          	&nbsp;&nbsp;&nbsp;&nbsp;
+
+    	    <!-- if user dislikes post, style button differently -->
+          	<i
+          	  <?php if (userDisliked($post['id'])): ?>
+          		  class="fa fa-thumbs-down dislike-btn"
+          	  <?php else: ?>
+          		  class="fa fa-thumbs-o-down dislike-btn"
+          	  <?php endif ?>
+          	  data-id="<?php echo $post['id'] ?>"></i>
+          	<span class="dislikes"><?php echo getDislikes($post['id']); ?></span>
+          </div>
+       	</div>
+       <?php endforeach ?>
+      </div>
+      <script src="scripts.js"></script>
+    </body>
+    </html>
